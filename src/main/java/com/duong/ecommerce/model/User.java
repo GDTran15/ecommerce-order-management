e5cf6dbdd00cad58ad270905e47e0e53e582c364
GDTran15 <trangiaiduong1512@gmail.com>
2026-08-12 01:07:32 +1000

    refactor: fix User model clean out JPA stuff

--- src/main/java/com/duong/ecommerce/model/User.java
+++ src/main/java/com/duong/ecommerce/model/User.java
@@ -7,11 +7,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 import java.time.LocalDate;
 
-@Entity
+@Component
 @Table(name = "users")
 @Data
 @NoArgsConstructor
@@ -20,34 +21,25 @@ import java.time.LocalDate;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
-    @Column(name = "username", unique = true ,nullable = false)
     @Size(min = 8, max = 20)
     @NotBlank
     private String username;
-    @Column(name = "password", nullable = false)
     @NotBlank
     private String password;
-    @Column(name = "email", unique = true, nullable = false)
     @Email
     @NotBlank
     private String email;
     @NotBlank
-    @Column(name = "first_name",  nullable = false)
     private String firstName;
     @NotBlank
-    @Column(name = "last_name",  nullable = false)
     private String lastName;
     @NotBlank
-    @Column(name = "phone_number", nullable = false)
     private String phoneNumber;
-    @NotBlank
-    @Column(name = "date_of_birth", nullable = false)
+    @NotNull
     private LocalDate dateOfBirth;
-    @NotBlank
-    @Column(name = "created_at", nullable = false,updatable = false)
+    @NotNull
     private Instant createdAt;
 
 }
