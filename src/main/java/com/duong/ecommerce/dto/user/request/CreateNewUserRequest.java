3324b43514241c88bf51df71a43001b5c1440bb5
GDTran15 <trangiaiduong1512@gmail.com>
2026-08-24 01:20:14 +1000

    refractor: add group validation into CreateNewUserRequest

--- src/main/java/com/duong/ecommerce/dto/user/request/CreateNewUserRequest.java
+++ src/main/java/com/duong/ecommerce/dto/user/request/CreateNewUserRequest.java
@@ -1,26 +1,31 @@
 package com.duong.ecommerce.dto.user.request;
 
+import com.duong.ecommerce.utility.OnCreate;
+import com.duong.ecommerce.utility.OnUpdate;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 
 import java.time.LocalDate;
 
 public record CreateNewUserRequest (
-        @NotBlank(message = "Username is required")
+        @NotBlank(message = "Username is required",groups = OnCreate.class)
+        @Size(min = 8, max = 20, message = "Username must be between 8 and 20 characters", groups = {OnCreate.class, OnUpdate.class})
         String username,
-        @NotBlank(message = "Password is required")
+        @NotBlank(message = "Password is required", groups = OnCreate.class)
         String password,
-        @NotBlank(message = "First name is required")
+        @NotBlank(message = "First name is required" ,groups = OnCreate.class)
         String firstName,
-        @NotBlank(message = "Last name is required")
+        @NotBlank(message = "Last name is required" ,groups = OnCreate.class)
         String lastName,
-        @NotBlank(message = "Email is required")
-        @Email(message = "Email is invalid")
+        @NotBlank(message = "Email is required",groups = OnCreate.class)
+        @Email(message = "Email is invalid", groups = {OnCreate.class, OnUpdate.class})
         String email,
-        @NotBlank(message = "Phone number is required")
+        @NotBlank(message = "Phone number is required", groups = OnCreate.class)
         String phoneNumber,
-        @NotNull(message = "Date of birth is required")
+        @NotNull(message = "Date of birth is required" ,groups = OnCreate.class)
         LocalDate dateOfBirth
 
 ){}
