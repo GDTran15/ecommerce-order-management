59b5ae662a64015c7e2de4890884f58adc177566
GDTran15 <trangiaiduong1512@gmail.com>
2026-08-24 01:24:52 +1000

    feat: create order entity

---
+++ src/main/java/com/duong/ecommerce/model/Order.java
@@ -0,0 +1,21 @@
+package com.duong.ecommerce.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Order {
+
+    private Long id;
+    private Long customerId;
+    private OrderStatus status;
+    private BigDecimal totalAmount;
+}
