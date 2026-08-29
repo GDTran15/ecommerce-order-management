b8241a977cf3cd968f7c2df299fec28279d664ce
GDTran15 <trangiaiduong1512@gmail.com>
2026-08-29 19:24:52 +1000

    feat: config jwt filter

--- src/main/java/com/duong/ecommerce/auth/JwtFilter.java
+++ src/main/java/com/duong/ecommerce/auth/JwtFilter.java
@@ -1,20 +1,72 @@
 package com.duong.ecommerce.auth;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
+
+
+    private final JwtService jwtService;
+
+    private final MyUserDetailsService myUserDetailsService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-          final String header = request.getHeader("Bearer ");
-          String jwtToken = header.substring(7);
+        final String header = request.getHeader("Authorization");
+        String token = null;
+        String username = null;
 
+        try {
+            if (header != null && header.startsWith("Bearer ")) {
+                token = header.substring(7);
+                username = jwtService.extractUsername(token);
+            }
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+                UserDetails userDetails = myUserDetailsService.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                SecurityContext context = SecurityContextHolder.createEmptyContext();
+                context.setAuthentication(auth);
+                SecurityContextHolder.setContext(context);
+
+            }
+        } catch (ExpiredJwtException e){
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            response.getWriter().write("""
+                    {
+                    "error_code" : "EXPIRED_TOKEN"
+     
+                    }
+                    """);
+
+            return;
+        }catch (JwtException | IllegalArgumentException e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            response.getWriter().write("""
+                    {
+                    "error_code" : "INVALID_TOKEN"
+                    }
+                    """);
+                return;
+
+            }
+            filterChain.doFilter(request,response);
     }
 }
