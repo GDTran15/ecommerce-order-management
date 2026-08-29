4e209f4d869b2f550ab7dedfe2749deafa15dea1
GDTran15 <trangiaiduong1512@gmail.com>
2026-08-29 19:54:59 +1000

    feat: create Role entity with RoleName enum, flyway design for for role tables and role users table

---
+++ src/main/java/com/duong/ecommerce/user/model/Role.java
@@ -0,0 +1,16 @@
+package com.duong.ecommerce.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+
+    private long id;
+    private RoleName name;
+}
---
+++ src/main/java/com/duong/ecommerce/user/model/RoleName.java
@@ -0,0 +1,5 @@
+package com.duong.ecommerce.user.model;
+
+public enum RoleName {
+    CUSTOMER,ADMIN,STAFF
+}
